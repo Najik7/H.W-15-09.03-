bc784598a37d3420987e30ad003b5be7c91a79e7
Najik7 <tajnaj27@gmail.com>
2021-03-10 20:02:22 +0800

    Add project files.

---
+++ H.W 15  (09.03)/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace H.W_15___09._03_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<int, string> people = new Dictionary<int, string>(3);
+            //same as
+            var people = new Dictionary<int, string>();
+
+            //Добавляем:
+            people.Add(1, "Alijon");
+            people.Add(2, "Malik");
+            people.Add(3, "Khamza");
+
+
+            //Выводим на консоль:
+            foreach (var item in people)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            
+            //same as:
+            people = new Dictionary<int, string>()
+            {
+                {1, "Anvar" },
+                {2, "Akram" },
+                {3, "Almal" },
+            };
+
+
+            //Удаление:
+            people.Remove(1);
+
+            foreach (KeyValuePair<int, string> item in people)
+            {
+                Console.WriteLine($"Пара ключей: {item.Key};  Пара значений: {item.Value}");
+            }
+            Console.WriteLine();
+
+            //
+            string name = people[2];
+            Console.WriteLine(name);
+
+            var key = people.Keys;
+            foreach (var item in key)
+            {
+                Console.WriteLine(key);
+            }
+
+
+            //Запрашиваем количество элементов в Dictionary
+            Console.WriteLine($"\n{people.Count}");
+
+
+
+        }
+    }
+}
